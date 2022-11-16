bb6569c230f842ba9e6f425410a91469b7d45447
Juan Carlos Valdés <idevkingos@gmail.com>
2022-11-15 22:37:28 -0300

    feat: arquitectura base

--- dist/auth/guards/roles.guard.d.ts
+++
@@ -1,8 +0,0 @@
-import { CanActivate, ExecutionContext } from '@nestjs/common';
-import { Reflector } from '@nestjs/core';
-import { Observable } from 'rxjs';
-export declare class RolesGuard implements CanActivate {
-    private reflactor;
-    constructor(reflactor: Reflector);
-    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean>;
-}
